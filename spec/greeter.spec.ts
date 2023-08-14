9cc08ad025606d7179ad881dbaf5497d7e98124d
Alan Wostenberg <pwostenberg@yahoo.com>
2023-08-14 17:43:31 +0000

    t1 from gh spaces

--- spec/greeter.spec.ts
+++ spec/greeter.spec.ts
@@ -1,5 +1,5 @@
 describe("hello world", () => {
     it.todo("greets by name")
     it.todo("greets by nickname if applicable")
-    it.todo("greets morning or evening depending on local time")
+    it.todo("greets morning or afternoon suitable to local time")
 })
