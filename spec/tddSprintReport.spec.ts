5c0bc5d318fa3cdc00fd9c1ae33b61fdfb0be262
Alan Wostenberg <alan.wostenberg@cvshealth.com>
2023-08-14 15:01:17 -0600

    t2 sprint report specs

---
+++ spec/tddSprintReport.spec.ts
@@ -0,0 +1,37 @@
+/* PSA. TDD Coaching. 9-Aug. Week I. TDD in states and moves 
+
+        I Midway in the sprint we've had 5 of 7 players join at least one 90 minute TDD joint practice session
+
+        II for 9 total manhours of practice, out of 35 reserved this sprint.
+
+ 
+
+        III All participants earned the 'initial test list' badge.
+
+        
+
+        IV. Thank you Payal, Sai, Radha, Swetha, and Kitty, for investing time to sharpen the saw, together.
+
+ 
+
+        V. Enrollment for TDD coaching is open. Book a time  .
+
+        
+
+        VI. DIY Kit https://tinyurl.com/5n7adan4
+
+ 
+
+    */
+describe("tdd sprint report", () => {
+    it.todo("I. reports fraction of players who participated, e.g. 5 of 7")
+    it.todo("IIa. reports total manhours of practice, e.g. people * hours")
+    it.todo("IIb. reserved hours, e.g. people * 5 hours")
+    it.todo("III. reports badges earned, e.g. all participatants earned /initial test/")
+    it.todo("IV. thanks participants by name, e.g. thank you Payal, Sai, Radha, Swetha and Kitty")
+    it.todo("V. enrollment open <url>")
+    it.todo("VI. do it yourself kit <url>")
+
+
+
+})
